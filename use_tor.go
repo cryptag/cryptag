cada1252e69cedd361c6aac955392b282d98a714
Steve Phillips <steve@tryingtobeawesome.com>
2016-04-18 12:18:08 -0700

    cpass-sandstorm, cpass-dropboxremote now do HTTP calls over Tor when TOR=1
    Further contributes to #17

---
+++ use_tor.go
@@ -0,0 +1,16 @@
+// Steven Phillips / elimisteve
+// 2016.04.18
+
+package cryptag
+
+import "os"
+
+// UseTor is one centralized place for all code to check to see if it
+// should do HTTP calls over Tor.
+var UseTor = false
+
+func init() {
+	if os.Getenv("TOR") == "1" {
+		UseTor = true
+	}
+}
