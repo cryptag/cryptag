b809b801bb69c5f620018331bc97dea4a27746b9
Steve Phillips <steve@tryingtobeawesome.com>
2016-05-12 05:51:48 -0700

    Paths now set correctly on Windows
    Fixes #41

--- path.go
+++ path.go
@@ -4,32 +4,41 @@
 package cryptag
 
 import (
+	"log"
 	"os"
 	"path"
 	"runtime"
+
+	homedir "github.com/mitchellh/go-homedir"
 )
 
 var (
+	home, errHome = homedir.Dir()
+
 	// TrustedBasePath is where private, non-shared data (e.g., crypto
 	// keys) can be safely stored.
-	TrustedBasePath = path.Join(os.Getenv("HOME"), ".cryptag")
+	TrustedBasePath = path.Join(home, ".cryptag")
 
 	// BackendPath is the path to the directory where backend config
 	// files are stored (e.g., "/home/myusername/.cryptag/backends").
 	// This can be overridden with the BACKEND_PATH
 	// environment variable (rarely useful; this exists so that all
 	// CrypTag backend configs could be on a USB drive).
-	BackendPath = path.Join(os.Getenv("HOME"), ".cryptag", "backends")
+	BackendPath = path.Join(home, ".cryptag", "backends")
 
 	// DefaultLocalDataPath is the default directory where local
 	// backends will be told to store their data.  This directory will
 	// contain 'rows' and 'tags' subdirectories.
-	DefaultLocalDataPath = path.Join(os.Getenv("HOME"), ".cryptag")
+	DefaultLocalDataPath = path.Join(home, ".cryptag")
 
 	LocalDataPath = DefaultLocalDataPath
 )
 
 func init() {
+	if errHome != nil {
+		log.Printf("Error getting/setting home directory path: %v\n", err)
+	}
+
 	if p := os.Getenv("BACKEND_PATH"); p != "" {
 		BackendPath = p
 	}
