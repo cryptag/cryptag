ebe9a0af9ff1ad3206bc93cfb399185d7c9b47b8
Steve Phillips <steve@tryingtobeawesome.com>
2016-06-02 21:13:58 -0700

    mobile/tasks.go: Added TaskGetter type + tests
    Works with 9ba2b8bad54e2588ead7b0bf71715b47d93970b2 from CrypTask repo

--- mobile/tasks.go
+++ mobile/tasks.go
@@ -1,5 +1,10 @@
 package mobile
 
+import (
+	"fmt"
+	"sync"
+)
+
 /*
 type Counter struct {
 	Value int
@@ -24,4 +29,46 @@ type Task struct {
 	Title       string
 	Description string
 	Assignee    string
+}
+
+func (t *Task) IDStr() string {
+	return fmt.Sprintf("%v", t.ID)
+}
+
+func (t *Task) String() string {
+	if t == nil {
+		return "<nil>"
+	}
+	return fmt.Sprintf("%#v", t)
+}
+
+type TaskGetter struct {
+	tasks []*Task
+}
+
+var allTasks = []*Task{
+	{1, "Show demo CrypTask Android app to Sky!", "(Description 1)", "steve"},
+	{2, "Show this app to Gabrielle!", "(Description 2)", "steve"},
+	{3, "Tell AJ!", "(Description 3)", "steve"},
+	{4, "Show this off to Jim!", "(Description 4)", "steve"},
+	{5, "Shout out to Sam!", "(Description 5)", "steve"},
+}
+
+var taskLock sync.RWMutex
+
+func NewTaskGetter() *TaskGetter {
+	taskLock.Lock()
+	defer taskLock.Unlock()
+
+	tasks := make([]*Task, len(allTasks))
+	copy(tasks, allTasks)
+	return &TaskGetter{tasks: allTasks}
+}
+
+func (tg *TaskGetter) At(i int) *Task {
+	return tg.tasks[i]
+}
+
+func (tg *TaskGetter) Length() int {
+	return len(tg.tasks)
 }
