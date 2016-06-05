e0f31b194074c92cc14f7ef095372c41291f210b
Steve Phillips <steve@tryingtobeawesome.com>
2016-06-05 11:48:30 -0700

    mobile: Now fetches new Tasks, TagPairs from Webserver backend every 30 secs!

--- mobile/tasks.go
+++ mobile/tasks.go
@@ -1,38 +1,22 @@
 package mobile
 
 import (
+	"encoding/json"
 	"fmt"
-	"sync"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/elimisteve/cryptag/backend"
+	"github.com/elimisteve/cryptag/rowutil"
+	"github.com/elimisteve/cryptag/types"
 )
 
-/*
-type Counter struct {
-	Value int
-}
-
-func (c *Counter) Inc() { c.Value++ }
-
-func New() *Counter { return &Counter{5} }
-*/
-
-func NewTask(id int, title, description, assignee string) *Task {
-	return &Task{
-		ID:          id,
-		Title:       title,
-		Description: description,
-		Assignee:    assignee,
-	}
-}
-
 type Task struct {
-	ID          int
+	ID          string `json:"-"` // Save as tag, not in Row.decrypted
 	Title       string
 	Description string
-	Assignee    string
-}
-
-func (t *Task) IDStr() string {
-	return fmt.Sprintf("%v", t.ID)
+	Assignee    string `json:"-"` // Save as tag, not in Row.decrypted
 }
 
 func (t *Task) String() string {
@@ -42,23 +26,87 @@ func (t *Task) String() string {
 	return fmt.Sprintf("%#v", t)
 }
 
+var (
+	bk        *backend.WebserverBackend
+	taskCh    = make(chan types.Rows)
+	tagPairCh = make(chan types.TagPairs)
+
+	plainTagsGet = []string{
+		"app:cryptask",
+		"type:task",
+	}
+)
+
+func init() {
+	key := []byte{}
+	ws, err := backend.NewWebserverBackend(
+		key,
+		"sandstorm-cryptask",
+		"BASE_URL",
+		"API_KEY",
+	)
+	if err != nil {
+		log.Fatalf("Error from NewWebserverBackend: %v\n", err)
+	}
+	bk = ws
+
+	go func() {
+		pairs, _ := bk.AllTagPairs()
+		log.Printf("AllTagPairs initially fetched %d pairs\n", len(pairs))
+		tagPairCh <- pairs
+
+		tick := time.Tick(30 * time.Second)
+		for {
+			select {
+			case tagPairCh <- pairs:
+				// Pass to polling goroutine, below
+			case <-tick:
+				// Fetch latest TagPairs
+				latestPairs, err := bk.AllTagPairs()
+				if err != nil {
+					log.Printf("Error from AllTagPairs: %v\n", err)
+					continue
+				}
+				log.Printf("Just fetched %d TagPairs\n", len(latestPairs))
+				pairs = latestPairs
+			}
+		}
+	}()
+
+	go func() {
+		taskRows, _ := backend.RowsFromPlainTags(bk, <-tagPairCh, plainTagsGet)
+		log.Printf("RowsFromPlainTags initially fetched %d task rows\n",
+			len(taskRows))
+		taskCh <- taskRows
+
+		tick := time.Tick(30 * time.Second)
+		for {
+			select {
+			case taskCh <- taskRows:
+				// Passed most-recently-fetched tasks to
+				// NewTaskGetter()
+			case <-tick:
+				pairs := <-tagPairCh
+				rows, err := backend.RowsFromPlainTags(bk, pairs, plainTagsGet)
+				if err != nil {
+					log.Printf("Error from RowsFromPlainTags: %v\n", err)
+					continue
+				}
+				log.Printf("Just fetched %d *Rows\n", len(rows))
+				taskRows = rows
+			}
+		}
+	}()
+}
+
 type TaskGetter struct {
 	tasks []*Task
 }
 
-var allTasks = []*Task{
-	{1, "Show demo CrypTask Android app to Sky!", "(Description 1)", "steve"},
-	{2, "Show this app to Gabrielle!", "(Description 2)", "steve"},
-	{3, "Tell AJ!", "(Description 3)", "steve"},
-	{4, "Show this off to Jim!", "(Description 4)", "steve"},
-	{5, "Shout out to Sam!", "(Description 5)", "steve"},
-}
-
-var taskLock sync.RWMutex
-
 func NewTaskGetter() *TaskGetter {
-	taskLock.Lock()
-	defer taskLock.Unlock()
+	rows := <-taskCh
+	rows.Sort(rowutil.ByTagPrefix("created:", true))
+	allTasks := tasksFromRows(rows)
 
 	tasks := make([]*Task, len(allTasks))
 	copy(tasks, allTasks)
@@ -71,4 +119,29 @@ func (tg *TaskGetter) At(i int) *Task {
 
 func (tg *TaskGetter) Length() int {
 	return len(tg.tasks)
+}
+
+func tasksFromRows(rows []*types.Row) []*Task {
+	tasks := make([]*Task, 0, len(rows))
+	for i, row := range rows {
+		var t Task
+		err := json.Unmarshal(row.Decrypted(), &t)
+		if err != nil {
+			log.Printf("Error unmarshaling task row: %v\n", err)
+			continue
+		}
+
+		// Row tagged with "assignee:alice" and "assignee:bob" -> "alice, bob"
+		names := rowutil.TagsWithPrefixStripped(row, "assignee:")
+		assignees := strings.Join(names, ", ")
+
+		t.Assignee = assignees
+
+		// t.ID = rowutil.TagWithPrefixStripped(row, "id:")
+		t.ID = fmt.Sprintf("%d", i+1)
+
+		tasks = append(tasks, &t)
+	}
+	log.Printf("tasksFromRows: returning %d *Task objects\n", len(tasks))
+	return tasks
 }
