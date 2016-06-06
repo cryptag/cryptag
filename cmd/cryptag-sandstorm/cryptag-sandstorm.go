a8a520bcb859790d0b8a895ef2d3dd0508a26e0b
Steve Phillips <steve@tryingtobeawesome.com>
2016-06-06 00:04:35 -0700

    cryptag-sandstorm: Dramatically cleaner; uses several high-level helpers

--- cmd/cryptag-sandstorm/cryptag-sandstorm.go
+++ cmd/cryptag-sandstorm/cryptag-sandstorm.go
@@ -5,18 +5,17 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
 	"path"
 	"path/filepath"
-	"regexp"
-	"strconv"
 	"strings"
 
 	"github.com/elimisteve/cryptag"
 	"github.com/elimisteve/cryptag/backend"
+	"github.com/elimisteve/cryptag/cli"
 	"github.com/elimisteve/cryptag/cli/color"
+	"github.com/elimisteve/cryptag/keyutil"
 	"github.com/elimisteve/cryptag/rowutil"
 )
 
@@ -56,7 +55,8 @@ func main() {
 		if len(os.Args) < 3 {
 			log.Fatalf("%s\n%s\n", initUsage, usage)
 		}
-		if err := createBackendConfig(os.Args[2]); err != nil {
+		webkey := os.Args[2]
+		if err := cli.InitSandstorm(backendName, webkey); err != nil {
 			log.Fatal(err)
 		}
 
@@ -68,38 +68,35 @@ func main() {
 
 		createFile := (os.Args[1] == "createfile")
 
-		var rowData []byte
 		tags := append(os.Args[3:], "app:cryptag")
 
 		if createFile {
 			filename := os.Args[2]
 
-			b, err := ioutil.ReadFile(filename)
+			row, err := backend.CreateFileRow(db, nil, filename, tags)
 			if err != nil {
-				log.Fatalf("Error reading file `%s`: %v\n", filename, err)
+				log.Fatalf("Error creating then saving file: %v", err)
 			}
 
-			rowData = b
-			tags = append(tags, "type:file", "filename:"+filepath.Base(filename))
-		} else {
-			rowData = []byte(os.Args[2])
-			tags = append(tags, "type:text")
+			color.Printf("Successfully saved new file %s with these tags:"+
+				"\n%v\n", filepath.Base(filename), color.Tags(row.PlainTags()))
+			return
 		}
 
-		row, err := backend.CreateRow(db, nil, rowData, tags)
+		// Create text row
+
+		text := os.Args[2]
+		tags = append(tags, "type:text")
+
+		row, err := backend.CreateRow(db, nil, []byte(text), tags)
 		if err != nil {
-			log.Fatalf("Error creating then saving new row: %v", err)
+			log.Fatalf("Error creating text: %v\n", err)
 		}
 
-		if createFile {
-			color.Printf("Successfully saved new row with these tags:\n%v\n",
-				color.Tags(row.PlainTags()))
-		} else {
-			color.Println(color.TextRow(row))
-		}
+		color.Println(color.TextRow(row))
 
 	case "getkey":
-		fmt.Println(fmtKey(db.Key()))
+		fmt.Println(keyutil.Format(db.Key()))
 
 	case "setkey":
 		if len(os.Args) < 3 {
@@ -109,20 +106,9 @@ func main() {
 
 		keyStr := strings.Join(os.Args[2:], ",")
 
-		newKey, err := parseKey(keyStr)
+		err := backend.UpdateKey(db, keyStr)
 		if err != nil {
-			log.Fatalf("Error from parseKey: %v\n", err)
-		}
-
-		cfg, err := db.ToConfig()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		cfg.Key = newKey
-
-		if err := cfg.Update(cryptag.BackendPath); err != nil {
-			log.Fatalf("Error updating config: %v", err)
+			log.Fatalf("Error updating config with new key: %v", err)
 		}
 
 	case "list":
@@ -217,68 +203,3 @@ func main() {
 
 var usage = "Usage: " + filepath.Base(os.Args[0]) + " [create <yourpassword>] tag1 [tag2 ...]"
 var initUsage = "Usage: " + filepath.Base(os.Args[0]) + " init <sandstorm_key>"
-
-func createBackendConfig(key string) error {
-	info := strings.SplitN(key, "#", 2)
-	if len(info) < 2 {
-		return fmt.Errorf(
-			"Error parsing `%v` as Sandstorm key generated from Sandstorm's UI",
-			key)
-	}
-
-	serverBaseURL, authToken := info[0], info[1]
-
-	db, err := backend.NewWebserverBackend(nil, backendName, serverBaseURL, authToken)
-	if err != nil {
-		return fmt.Errorf("NewWebserverBackend error: %v\n", err)
-	}
-
-	cfg, err := db.ToConfig()
-	if err != nil {
-		return fmt.Errorf("Error getting backend config: %v\n", err)
-	}
-
-	err = cfg.Save(cryptag.BackendPath)
-	if err != nil && err != backend.ErrConfigExists {
-		return fmt.Errorf("Error saving backend config to disk: %v\n", err)
-	}
-
-	return nil
-}
-
-var keyRegex = regexp.MustCompile(`(\d+)`)
-
-func parseKey(cliDigits string) (*[32]byte, error) {
-	// Pluck out all digit sequences, convert to numbers
-	nums := keyRegex.FindAllString(cliDigits, -1)
-	if len(nums) != 32 {
-		return nil, fmt.Errorf("Key must include 32 numbers, not %d", len(nums))
-	}
-
-	var newKey [32]byte
-
-	for i := 0; i < 32; i++ {
-		n, err := strconv.ParseUint(nums[i], 10, 8)
-		if err != nil {
-			return nil, fmt.Errorf("Number #%d '%v' was invalid: %v\n", i+1,
-				nums[i])
-		}
-		newKey[i] = byte(n)
-	}
-
-	return &newKey, nil
-}
-
-func fmtKey(key *[32]byte) string {
-	if key == nil {
-		return "<nil>"
-	}
-	k := *key
-
-	kStr := fmt.Sprintf("%d", k[0])
-
-	for i := 1; i < len(k); i++ {
-		kStr += fmt.Sprintf(",%d", k[i])
-	}
-	return kStr
-}
