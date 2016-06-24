4752f52a7733d4d98b951bb08e0194e24dc3e11d
Steve Phillips <steve@tryingtobeawesome.com>
2016-06-24 02:36:50 -0700

    cryptagd: If LoadWebserverBackend fails, tries LoadOrCreateFileSystem

--- servers/cryptagd/cryptagd.go
+++ servers/cryptagd/cryptagd.go
@@ -31,13 +31,24 @@ func init() {
 }
 
 func main() {
+	var db backend.Backend
+
 	db, err := backend.LoadWebserverBackend("", backendName)
 	if err != nil {
-		log.Fatalf("Error from LoadWebserverBackend: %v", err)
+		// TODO: Generically parse all Backend Configs
+		log.Printf("Error from LoadWebserverBackend: %v", err)
+
+		db, err = backend.LoadOrCreateFileSystem(
+			os.Getenv("BACKEND_PATH"),
+			os.Getenv("BACKEND"),
+		)
+		if err != nil {
+			log.Fatalf("Error from LoadOrCreateFileSystem: %s", err)
+		}
 	}
 
-	if cryptag.UseTor {
-		if err = db.UseTor(); err != nil {
+	if bk, ok := db.(cryptag.CanUseTor); ok && cryptag.UseTor {
+		if err = bk.UseTor(); err != nil {
 			log.Fatalf("Error trying to use Tor: %v\n", err)
 		}
 	}
